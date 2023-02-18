1a10125cf5f579fe1efa6acc102467f156cc1e4b
Jatin Salve <66058203+jatinS-dev@users.noreply.github.com>
2023-02-18 10:55:35 +0530

    Time: 278 ms (47.00%), Space: 71 MB (98.03%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -1,16 +1,12 @@
 class Solution {
 private:
-    void dfs(TreeNode *p, TreeNode *q, int lvl)
-    {
+    void dfs(TreeNode *p, TreeNode *q, int lvl) {
         if(!p && !q)
             return;
         if(lvl % 2 && p && q)
             swap(p->val, q->val);
         
-        // swap the value of left's left children with value of right's right children
         dfs(p->left, q->right, lvl+1);
-
-        // swap the value of left's right children with value of right's left children
         dfs(p->right, q->left, lvl+1);
     }
 public:
